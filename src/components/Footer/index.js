e6de70a8950d854a331b0109cab56cc279c6c7a7
Emiliano <emilianop96@hotmail.com>
2021-08-23 02:56:31 -0300

    create footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <h4>© {new Date().getFullYear()} deConcesionarias</h4>
+    </footer>
+  );
+}
