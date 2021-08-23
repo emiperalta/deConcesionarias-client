0f631b6b4f2e64311e358a41648ba507ebfcd9ee
Emiliano <emilianop96@hotmail.com>
2021-08-23 02:56:16 -0300

    create header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <img
+        alt='deConcesionaria'
+        src='https://deconcesionarias.com.ar/wp-content/uploads/2021/04/deconcesionarias-logo-white.svg'
+      />
+    </header>
+  );
+}
