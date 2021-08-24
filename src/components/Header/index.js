178231934d49f2805b3d15b9ad3e1896ef9c75d7
Emiliano <emilianop96@hotmail.com>
2021-08-23 23:19:07 -0300

    add header links. add some styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,20 @@
+import { Link } from 'wouter';
+
 import './Header.css';
 
 export default function Header() {
   return (
-    <header className='header'>
-      <img
-        alt='deConcesionaria'
-        src='https://deconcesionarias.com.ar/wp-content/uploads/2021/04/deconcesionarias-logo-white.svg'
-      />
-    </header>
+    <>
+      <header className='header'>
+        <img
+          alt='deConcesionaria'
+          src='https://deconcesionarias.com.ar/wp-content/uploads/2021/04/deconcesionarias-logo-white.svg'
+        />
+        <section className='header-links'>
+          <Link to='/vehicles'>Vehículos</Link>
+          <Link to='/properties'>Propiedades de vehículos</Link>
+        </section>
+      </header>
+    </>
   );
 }
