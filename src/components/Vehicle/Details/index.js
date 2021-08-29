ef10d00ef715e4ebe82694f72d5f91f41c38aead
Emiliano <emilianop96@hotmail.com>
2021-08-29 14:59:21 -0300

    change some styles

--- src/components/Vehicle/Details/index.js
+++ src/components/Vehicle/Details/index.js
@@ -38,10 +38,15 @@ export default function VehicleDetails({ vehicle }) {
 
   return (
     <Container>
-      <Container className='mt-5'>
+      <Container className='mt-5 mb-4'>
+        <h2>
+          Detalles de vehículo: <span className='vehicle-name'>{vehicle.name}</span>
+        </h2>
+      </Container>
+      <Container className='mt-3 vehicle-details'>
         <div className='card'>
           <div className='card-body'>
-            <ul className='nav flex-column nav-pills'>
+            <ul className='nav flex-column nav-pills h-scroll'>
               {categories.length &&
                 categories.map(cat => (
                   <div
@@ -59,21 +64,23 @@ export default function VehicleDetails({ vehicle }) {
             </ul>
           </div>
         </div>
-      </Container>
-      <Container className='vehicle-details'>
         {propertiesToShow.length ? (
-          propertiesToShow.map(prop => (
-            <div key={prop.id} className='vehicle-properties-detail'>
-              <span>{prop.name}</span>
-              <RatingStar
-                initialValue={prop.Vehicles[0].PropertyValue.value}
-                propertyId={prop.id}
-                vehicleId={vehicle.id}
-              />
-            </div>
-          ))
+          <div className='vehicle-details-container'>
+            {propertiesToShow.map(prop => (
+              <div key={prop.id} className='vehicle-properties-detail'>
+                <p>{prop.name}</p>
+                <RatingStar
+                  initialValue={prop.Vehicles[0].PropertyValue.value}
+                  propertyId={prop.id}
+                  vehicleId={vehicle.id}
+                />
+              </div>
+            ))}
+          </div>
         ) : (
-          <section>¡No hay ninguna propiedad en esta categoría!</section>
+          <section className='no-properties text-center'>
+            ¡No hay ninguna propiedad en esta categoría!
+          </section>
         )}
       </Container>
       <div className='container back-arrow'>
